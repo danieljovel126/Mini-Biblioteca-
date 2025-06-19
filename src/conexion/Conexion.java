1c50f2dec1b3834367e9e2cb40e99854eccc275a
danieljovel126 <danieljovel656@gmail.com>
2025-06-19 14:54:12 -0500

    Conexion con JDBC

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca_libros";
     private static final String USUARIO = "root";
-    private static final String CONTRASENA = ""; // pon tu contraseña si tienes
+    private static final String CONTRASENA = ""; 
 
     public static Connection obtenerConexion() {
         Connection conexion = null;
@@ -15,12 +15,12 @@ public class Conexion {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
-            System.out.println("✅ Conexión exitosa a la base de datos.");
+            System.out.println("Conexión exitosa a la base de datos.");
         } catch (ClassNotFoundException e) {
-            System.out.println("❌ No se encontró el driver JDBC.");
+            System.out.println("No se encontró el driver JDBC.");
             e.printStackTrace();
         } catch (SQLException e) {
-            System.out.println("❌ Error al conectar con la base de datos.");
+            System.out.println("Error al conectar con la base de datos.");
             e.printStackTrace();
         }
 
